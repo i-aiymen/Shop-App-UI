2f7b1320add95f1db7f6c04c60ecbc6eac684417
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2022-01-26 20:59:40 +0530

    Added: Logo and Nav buttons

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,59 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
+        body: Column(children: [
+          Row(
+            children: [
+              SizedBox(width: 12,),
+              Text("Shop ", style: TextStyle(fontSize: 32, letterSpacing: 1)),
+              Text("Anthropologie", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, letterSpacing: 1))
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              buildFlatButton("Home decor", isSelected: true),
+              buildFlatButton("Bath & Body"),
+              buildFlatButton("Beauty"),
+              
+            ]
+          ),
+          SizedBox(height: 20),
+          Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[300],
+                      spreadRadius: 1,
+                      blurRadius: 20,
+                    ) 
+                  ]
+                ),
+                
+            ),
+          ),
+        ],
+        ),
       )
     );
+  }
+
+  FlatButton buildFlatButton(String text,{bool isSelected=false}) {
+    return FlatButton(
+              onPressed: (){
+                print("Button pressed");
+              }, 
+              child: Text(
+                text,
+                style: TextStyle(color: isSelected ? Colors.white : Colors.black,fontSize: 18),  
+              ),
+              shape: StadiumBorder(),
+              color: isSelected ? Colors.pink[100]: Colors.grey[300],
+            );
   }
 }
 
